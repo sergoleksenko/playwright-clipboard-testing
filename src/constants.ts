1ae07167775d1594052c593ed4b34f229d005b8b
Sergii Oleksenko <serg.oleksenko@gmail.com>
2026-08-31 10:56:40 +0200

    extracted firefoxClipboardPrefs into constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const firefoxClipboardPrefs = {
+  'dom.events.asyncClipboard.readText': true,
+  'dom.events.asyncClipboard.clipboardData': true,
+  'dom.events.testing.asyncClipboard': true,
+};
