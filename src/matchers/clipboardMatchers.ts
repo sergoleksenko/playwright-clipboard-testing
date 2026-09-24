10922384261028d976978941d2cd8987b20fe26f
Sergii Oleksenko <serg.oleksenko@gmail.com>
2026-09-24 16:18:03 +0200

    fixed type ClipboardMatchers

--- src/matchers/clipboardMatchers.ts
+++ src/matchers/clipboardMatchers.ts
@@ -5,31 +5,40 @@ import { toHaveContentLength } from './toHaveContentLength';
 import { toHaveJSONContent } from './toHaveJSONContent';
 import { toHaveTextContent } from './toHaveTextContent';
 import { toMatchJSONContent } from './toMatchJSONContent';
+import type { IgnoreCaseMatcherOptions, TimeoutMatcherOptions, TrimMatcherOptions } from './types';
 
 /**
  * Define the type of the clipboard matchers for TypeScript.
  */
 export type ClipboardMatchers = {
-  toBeBlank(this: ExpectMatcherState, clipboard: ClipboardHandler): Promise<MatcherReturnType>;
+  toBeBlank(
+    this: ExpectMatcherState,
+    clipboard: ClipboardHandler,
+    options?: TimeoutMatcherOptions & TrimMatcherOptions,
+  ): Promise<MatcherReturnType>;
   toHaveContentLength(
     this: ExpectMatcherState,
     clipboard: ClipboardHandler,
     expected: number,
+    options?: TimeoutMatcherOptions & TrimMatcherOptions,
   ): Promise<MatcherReturnType>;
   toHaveJSONContent(
     this: ExpectMatcherState,
     clipboard: ClipboardHandler,
     expected: unknown,
+    options?: TimeoutMatcherOptions,
   ): Promise<MatcherReturnType>;
   toHaveTextContent(
     this: ExpectMatcherState,
     clipboard: ClipboardHandler,
     expected: string | RegExp,
+    options?: TimeoutMatcherOptions & IgnoreCaseMatcherOptions & TrimMatcherOptions,
   ): Promise<MatcherReturnType>;
   toMatchJSONContent(
     this: ExpectMatcherState,
     clipboard: ClipboardHandler,
     expected: unknown,
+    options?: TimeoutMatcherOptions,
   ): Promise<MatcherReturnType>;
 };
 
