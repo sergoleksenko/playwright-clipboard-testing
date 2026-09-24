b149a3cbb560b350a322aab9b13ff4e5886ec82b
Sergii Oleksenko <serg.oleksenko@gmail.com>
2026-09-24 14:29:57 +0200

    removed matchers export

--- src/matchers/index.ts
+++ src/matchers/index.ts
@@ -1,10 +1,4 @@
 import { type ClipboardMatchers, clipboardMatchers } from './clipboardMatchers';
 
-export * from './toBeBlank';
-export * from './toHaveContentLength';
-export * from './toHaveJSONContent';
-export * from './toHaveTextContent';
-export * from './toMatchJSONContent';
-
 export type { ClipboardMatchers };
 export { clipboardMatchers };
