e2a7f8f212a6a8196c1efad3c89ae2e7760f121d
Sergii Oleksenko <serg.oleksenko@gmail.com>
2026-09-24 16:29:08 +0200

    exported options types

--- src/matchers/index.ts
+++ src/matchers/index.ts
@@ -1,4 +1,10 @@
 import { type ClipboardMatchers, clipboardMatchers } from './clipboardMatchers';
+import type { IgnoreCaseMatcherOptions, TimeoutMatcherOptions, TrimMatcherOptions } from './types';
 
-export type { ClipboardMatchers };
+export type {
+  ClipboardMatchers,
+  IgnoreCaseMatcherOptions,
+  TimeoutMatcherOptions,
+  TrimMatcherOptions,
+};
 export { clipboardMatchers };
